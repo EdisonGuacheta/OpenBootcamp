352dd865f1b731d2a69c6552db8b956e662fe78d
Rextsy <rextsy@localhost.localdomain>
2022-09-06 07:06:58 -0500

    subida del protecto

---
+++ src/App.java
@@ -0,0 +1,35 @@
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        double x = 2;
+        double y = 3;
+        double z = y + x;
+
+        var suma  = Suma(x,y,z);
+
+        coche miCoche = new coche();
+
+        miCoche.maspuerta();
+
+        System.out.println("la suma de " + x + " + " + y + " + " + z + " da: " + suma );
+        System.out.println(miCoche.puertas);
+    }
+
+    public static double Suma( Double x, Double y, Double z){
+
+        return x + y + z ;
+    }
+}
+
+class coche{
+    public double puertas = 5;
+
+    public void maspuerta(){
+        puertas = puertas + 1;
+    }
+
+    public void menospuerta(){
+        puertas = puertas - 1;
+    }
+
+}
